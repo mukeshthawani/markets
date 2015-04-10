0990da6ccbebca2f8837d2da2449db0043bc7cb7
mukeshthawani <mukesh9039@gmail.com>
2015-04-10 11:26:04 +0530

    change url

--- markets/urls.py
+++ markets/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from stocks import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'markets.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.search, name='search'),
     url(r'^stocks/', include('stocks.urls', namespace="stocks")),
     url(r'^admin/', include(admin.site.urls)),
 )
